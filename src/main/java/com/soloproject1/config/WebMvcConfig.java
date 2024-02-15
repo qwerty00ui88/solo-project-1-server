285fd139e165dfc00f1afa621952b15d5d457950
hameee <sohui.dev@gmail.com>
2024-02-15 22:07:29 +0900

    feat: 권한 설정

--- src/main/java/com/soloproject1/config/WebMvcConfig.java
+++ src/main/java/com/soloproject1/config/WebMvcConfig.java
@@ -1,12 +1,19 @@
 package com.soloproject1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.soloproject1.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Autowired
+	private PermissionInterceptor interceptor;
+	
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -14,5 +21,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true);
     }
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry
+		.addInterceptor(interceptor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/static/**", "/error");
+	}
 	
 }
