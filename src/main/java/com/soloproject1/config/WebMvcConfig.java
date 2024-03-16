9802859ea3e7b404f4afd5bff44a571293ccb359
hameee <sohui.dev@gmail.com>
2024-03-16 14:44:23 +0900

    deploy: allowedOrigins 수정

--- src/main/java/com/soloproject1/config/WebMvcConfig.java
+++ src/main/java/com/soloproject1/config/WebMvcConfig.java
@@ -24,7 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://solo-project-1-c8107.web.app", "https://goodorbad.site", "http://goodorbad.site")
+		registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://solo-project-1-c8107.web.app")
 				.allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true);
 	}
 
